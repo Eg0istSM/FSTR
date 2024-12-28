923dc95b769587f39d34973416c07d16c694770b
Eg0istSM <Frolov27101993@yandex.ru>
2024-12-28 19:59:26 +0300

    created a project with the names of the models

---
+++ FSTR/pereval/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Pass(models.Model):
+    pass
+
+
+class User(models.Model):
+    pass
+
+
+class Coords(models.Model):
+    pass
+
+
+class Level(models.Model):
+    pass
+
+
+class Images(models.Model):
+    pass
