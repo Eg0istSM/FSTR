d801b70e8e98e5c221e3013d2f8243f661a0be77
Eg0istSM <Frolov27101993@yandex.ru>
2024-12-31 00:21:51 +0300

    rest_framework works

--- FSTR/FSTR/urls.py
+++ FSTR/FSTR/urls.py
@@ -15,8 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from pereval import views
+
+router = routers.DefaultRouter()
+router.register(r'user', views.UserViewSet, basename='user')
+router.register(r'coords', views.CoordsViewSet, basename='coords')
+router.register(r'level', views.LevelViewSet, basename='level')
+router.register(r'pereval', views.PerevalViewSet, basename='pereval')
+router.register(r'images', views.ImagesViewSet, basename='images')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api/', include('rest_framework.urls', namespace='rest_framework')),
+
 ]
